05685b0040e90ef0ce669ace8c80d6959f462a1b
Valerio Cofano <cofano@medacta.ch>
2024-03-21 15:17:07 +0100

    feat: first test

---
+++ tests/test_shortener.py
@@ -0,0 +1,15 @@
+
+class TestShortener:
+
+    long_url = "https://www.example.com/path?q=search"
+    short_base_url = "http://myurlshortener.com/"
+    shortener = Shortener(short_base_url)
+
+    def test_can_initialize_shortener(self):
+        assert self.shortener is not None
+
+    def test_can_minify_url(self):
+        short_url = self.shortener.minify(self.long_url)
+        assert short_url == "https://sho.rt/abc123"
+
+
