1c743b56f7c4bf252307c6afe2bbaf1071614257
Valerio Cofano <cofano@medacta.ch>
2024-03-21 16:19:47 +0100

    feat: can minify url

--- tests/test_shortener.py
+++ tests/test_shortener.py
@@ -4,7 +4,7 @@ from src.shortener import Shortener
 
 class TestShortener:
 
-    long_url = "https://www.example.com/path?q=search"
+    long_url = "https://www.example.com/path?q=searcah"
     short_base_url = "http://myurlshortener.com/"
     shortener = Shortener(short_base_url)
 
@@ -13,6 +13,9 @@ class TestShortener:
 
     def test_can_minify_url(self):
         short_url = self.shortener.minify(self.long_url)
-        assert short_url == "https://sho.rt/abc123"
+
+        assert short_url is not None
+        assert short_url.startswith(self.short_base_url)
+
 
 
